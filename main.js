2cb6c7362854e98418f10177d1cd7c68cd05e41b
werronense <31091965+werronense@users.noreply.github.com>
2020-02-03 08:07:15 -0800

    Refactor Book constructor

--- main.js
+++ main.js
@@ -7,37 +7,27 @@ function Book(title, author, pages, read = false) {
   this.title = title,
   this.author = author,
   this.pages = pages,
-  this.read = read,
-
-  this.info = function() {
-    return (
-      `${title} by ${author}, ${pages} pages, ${read ? '' : 'not yet'} read`
-    );
-  }
+  this.read = read
 }
 
 
-function addBookToLibrary(title, author, pages, read = false) {
-  myLibrary.push(new Book(title, author, pages, read));
-}
-
-
-function renderBook(book) {
+Book.prototype.getHtml = function() {
   let libraryItem = document.createElement("div");
+  libraryItem.classList.add("book");
 
   let title = document.createElement("h2");
-  title.textContent = book.title;
+  title.textContent = this.title;
 
   let info = document.createElement("ul");
 
   let author = document.createElement("li");
-  author.textContent = `Author: ${book.author}`;
+  author.textContent = `Author: ${this.author}`;
 
   let pages = document.createElement("li");
-  pages.textContent = `Pages: ${book.pages}`;
+  pages.textContent = `Pages: ${this.pages}`;
 
   let read = document.createElement("li");
-  read.textContent = `${read ? '' : 'not yet'} read`;
+  read.textContent = `${this.read ? '' : 'not yet'} read`;
 
   libraryItem.append(title);
 
@@ -51,16 +41,23 @@ function renderBook(book) {
 }
 
 
+function addBookToLibrary(title, author, pages, read = false) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+
 function render(library) {
   const allBooks = document.getElementById("all-books");
 
   library.forEach(book => {
-    allBooks.append(renderBook(book));
+    allBooks.append(book.getHtml());
   });
 }
 
 // temporary
 addBookToLibrary("The Hobbit", "J.R.R. Tolkein", 295, false);
+addBookToLibrary("The Brothers Karamazov", "Fyodor Dostoyevsky", 776, true);
+addBookToLibrary("Jane Eyre", "Charlotte Bronte", 502, false);
 // console.log(myLibrary);
 
 window.onload = () => {
